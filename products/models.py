342ebb3f303809b8709e7229d7046ce8a9d3d962
Wislon <wislon20@gmail.com>
2021-01-31 20:42:29 -0300

    criado ProductQuery setando na classe e definindo, ativando e melhorando o metodo inside da classe, extend get_queryset metodo, criado todos os metodos e melhorado o metodo dentrodo ProductManager da classe no src/products/models.py

--- products/models.py
+++ products/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 
+# Custom queryset
+
+
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+    def featured(self):
+        return self.filter(featured=True, active=True)
+
+
 class ProductManager(models.Manager):
+
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def all(self):
+        return self.get_queryset().active()
+
+    def featured(self):
+        # return self.get_queryset().filter(featured = True)
+        return self.get_queryset().featured()
+
     def get_by_id(self, id):
         qs = self.get_queryset().filter(id=id)
         if qs.count() == 1:
@@ -8,19 +30,23 @@ class ProductManager(models.Manager):
         return None
 
 # Create your models here.
+
+
 class Product(models.Model):  # product_category
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(
         decimal_places=2, max_digits=20, default=100.00)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    image = models.FileField(upload_to='products/', null=True, blank=True)
+    featured = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
 
     objects = ProductManager()
 
     # python 3
     def __str__(self):
         return self.title
-
     # python 2
+
     def __unicode__(self):
         return self.title
