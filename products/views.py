5c53ff6552898e3fa5a0a285bf0899776eb29e55
Wislon <wislon20@gmail.com>
2021-01-31 18:38:44 -0300

    atualizado src/products/views.py e criado o src/products/templates/products/detail.html

--- products/views.py
+++ products/views.py
@@ -1,21 +1,25 @@
 from django.views.generic import ListView, DetailView
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
 # Class Based View
+
+
 class ProductListView(ListView):
     # traz todos os produtos do banco de dados sem filtrar nada
     queryset = Product.objects.all()
     template_name = "products/list.html"
 
-    #def get_context_data(self, *args, **kwargs):
-    #context = super(ProductListView, self).get_context_data(
-    #*args, **kwargs)
-    #print(context)
-    #return context
+    # def get_context_data(self, *args, **kwargs):
+    # context = super(ProductListView, self).get_context_data(
+    # *args, **kwargs)
+    # print(context)
+    # return context
 
 # Function Based View
+
+
 def product_list_view(request):
     queryset = Product.objects.all()
     context = {
@@ -23,21 +27,28 @@ def product_list_view(request):
     }
     return render(request, "products/list.html", context)
 
-#Class Based View
+# Class Based View
+
+
 class ProductDetailView(DetailView):
-    #traz todos os produtos do banco de dados sem filtrar nada 
+    # traz todos os produtos do banco de dados sem filtrar nada
     queryset = Product.objects.all()
     template_name = "products/detail.html"
-    
-    #def get_context_data(self, *args, **kwargs):
-        #context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
-        #print(context)
-        #return context
 
-#Function Based View
-def product_detail_view(request):
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(
+            *args, **kwargs)
+        print(context)
+        return context
+
+# Function Based View
+
+
+def product_detail_view(request, pk=None, *args, **kwargs):
+    # instance = Product.objects.get(pk = pk) #get the object id
+    instance = get_object_or_404(Product, pk=pk)
     queryset = Product.objects.all()
     context = {
-        'object_list': queryset
+        'object': instance
     }
     return render(request, "products/detail.html", context)
