803523bd687617563a34242ddb9d9f4d723473cd
Wislon <wislon20@gmail.com>
2021-01-31 19:48:10 -0300

    importei o http404 e o handle 404 error com o try except clauses no src/products/views.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 
@@ -47,7 +48,11 @@ class ProductDetailView(DetailView):
 def product_detail_view(request, pk=None, *args, **kwargs):
     # instance = Product.objects.get(pk = pk) #get the object id
     instance = get_object_or_404(Product, pk=pk)
-    queryset = Product.objects.all()
+    try:
+        instance = Product.objects.get(id=pk)
+    except Product.DoesNotExist:
+        print("Nenhum produto encontrado aqui!")
+        raise Http404("Esse produto não existe!")
     context = {
         'object': instance
     }
