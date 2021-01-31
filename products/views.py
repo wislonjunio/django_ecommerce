33a5f64447771bb679cbfa59f3e269b1ab4796f5
Wislon <wislon20@gmail.com>
2021-01-31 18:16:51 -0300

    atualizando o pycache e products/views.py

--- products/views.py
+++ products/views.py
@@ -1,11 +1,9 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 
 from .models import Product
 
 # Class Based View
-
-
 class ProductListView(ListView):
     # traz todos os produtos do banco de dados sem filtrar nada
     queryset = Product.objects.all()
@@ -24,3 +22,22 @@ def product_list_view(request):
         'object_list': queryset
     }
     return render(request, "products/list.html", context)
+
+#Class Based View
+class ProductDetailView(DetailView):
+    #traz todos os produtos do banco de dados sem filtrar nada 
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+    
+    #def get_context_data(self, *args, **kwargs):
+        #context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        #print(context)
+        #return context
+
+#Function Based View
+def product_detail_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, "products/detail.html", context)
