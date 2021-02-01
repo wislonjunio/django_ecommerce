982c9e0a95f53acad5ed8823c29dd32579ae80f9
Wislon <wislon20@gmail.com>
2021-01-31 21:01:21 -0300

    Criado o products/templates/products/featured-detail.html

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -4,23 +4,23 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
+from products.views import (ProductListView, product_list_view, ProductDetailView, product_detail_view, ProductFeaturedListView, ProductFeaturedDetailView)
 from .views import home_page, about_page, contact_page, login_page, register_page
 
-urlpatterns = [
+urlpatterns=[
     path('', home_page),
-    path('about/', about_page), 
+    path('about/', about_page),
     path('contact/', contact_page),
     path('login/', login_page),
     path('register/', register_page),
+    path('featured/', ProductFeaturedListView.as_view()),
+    path('featured/<int:pk>/', ProductFeaturedDetailView.as_view()),
     path('products/', ProductListView.as_view()),
     path('products-fbv/', product_list_view),
     path('products/<int:pk>', ProductDetailView.as_view()),
-    path('products-fbv/<int:pk>', product_detail_view), 
+    path('products-fbv/<int:pk>', product_detail_view),
     path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
-    urlpatterns = urlpatterns + \
-        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns = urlpatterns + \
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns= urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns= urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
