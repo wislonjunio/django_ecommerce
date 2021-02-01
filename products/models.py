8584e2388ce74ac2c2da8299bae7a860c51bc293
Wislon <wislon20@gmail.com>
2021-01-31 21:30:44 -0300

    Configurado unique = True no Slug Field

--- products/models.py
+++ products/models.py
@@ -34,7 +34,7 @@ class ProductManager(models.Manager):
 
 class Product(models.Model):  # product_category
     title = models.CharField(max_length=120)
-    slug = models.SlugField()
+    slug = models.SlugField(unique = True)
     description = models.TextField()
     price = models.DecimalField(
         decimal_places=2, max_digits=20, default=100.00)
